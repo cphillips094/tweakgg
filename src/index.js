320047a80ba13a108c59ce959d3be7a665278311
Chris <cphillips094@gmail.com>
2021-03-15 16:29:10 -0400

    Add basic view pager

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom'
+import React, { useRef } from 'react'
+import clamp from 'lodash-es/clamp'
+import { useSprings, animated } from 'react-spring'
+import { useDrag } from 'react-use-gesture'
+import './view-pager.css'
+import './index.css'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+const pages = [
+	<React.Fragment></React.Fragment>,
+	<React.Fragment></React.Fragment>,
+	<React.Fragment></React.Fragment>,
+	<React.Fragment></React.Fragment>,
+]
+
+function Viewpager() {
+	const index = useRef(0)
+	const [props, set] = useSprings(pages.length, (i) => ({
+		x: i * window.innerWidth,
+		scale: 1,
+		display: 'block'
+	}))
+	const bind = useDrag(({ active, movement: [mx], direction: [xDir], distance, cancel }) => {
+		if (active && distance > window.innerWidth / 2)
+			cancel((index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, pages.length - 1)))
+		set((i) => {
+			if (i < index.current - 1 || i > index.current + 1) {
+				return { display: 'none' }
+			}
+			const x = (i - index.current) * window.innerWidth + (active ? mx : 0)
+			const scale = active ? 1 - distance / window.innerWidth / 2 : 1
+			return { x, scale, display: 'block' }
+		})
+	})
+	return props.map(({ x, display, scale }, i) => (
+		<animated.div {...bind()} key={i} style={{ display, x }}>
+			<animated.div style={{ scale }}>
+
+			</animated.div>
+		</animated.div>
+	))
+}
+
+render(<Viewpager />, document.getElementById('root'));
