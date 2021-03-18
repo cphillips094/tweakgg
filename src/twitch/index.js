158d1c3c4099deae6736f4d90b21f89ad2008097
Chris <cphillips094@gmail.com>
2021-03-18 15:20:41 -0400

    Fetch and use Twitch data on Twitch page

--- src/twitch/index.js
+++ src/twitch/index.js
@@ -1,10 +1,121 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { TwitchChat, TwitchPlayer } from 'react-twitch-embed';
 import Slider from '../side-scroller';
 import "./index.css";
 
 const Index = () => {
+	const [token, setToken] = useState('');
+	const [streamTitle, setStreamTitle] = useState('');
+	const [clips, setClips] = useState([]);
+	useEffect(() => { fetchToken() }, []);
+	useEffect(() => { fetchStreamTitle() }, [token]);
+	useEffect(() => { fetchClips() }, [token]);
+
+	const fetchToken = async () => {
+		try {
+			//setFetchingItems(true);
+			const response = await fetch(
+				'https://id.twitch.tv/oauth2/token?client_id=eyt3nit0oell5gdmi1jvr9mlmbjvrn&client_secret=pviql3gqkrhwe4sh0u0tbv59hadnq1&grant_type=client_credentials',
+				{ method: 'POST' }
+			);
+			const json = await response.json();
+			if (!response.ok || json.message) {
+				throw json.message || 'Something went wrong';
+			}
+			setToken(json.access_token);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			//setFetchingItems(false);
+		}
+	}
+
+	const fetchStreamTitle = async () => {
+		try {
+			//setFetchingItems(true);
+			const response = await fetch(
+				'https://api.twitch.tv/helix/channels?broadcaster_id=158130480',
+				{
+					method: 'GET',
+					headers: {
+						'client-id': 'eyt3nit0oell5gdmi1jvr9mlmbjvrn',
+						'Authorization': `Bearer ${token}`
+					}
+				}
+			);
+			const json = await response.json();
+			if (!response.ok || json.message) {
+				throw json.message || 'Something went wrong';
+			}
+			setStreamTitle(parseChannelData(json));
+		} catch (error) {
+			console.log(error);
+		} finally {
+			//setFetchingItems(false);
+		}
+	}
+
+	const fetchClips = async () => {
+		if (token) {
+			try {
+				//setFetchingItems(true);
+				const response = await fetch(
+					'https://api.twitch.tv/helix/clips?broadcaster_id=158130480',
+					{
+						method: 'GET',
+						headers: {
+							'client-id': 'eyt3nit0oell5gdmi1jvr9mlmbjvrn',
+							'Authorization': `Bearer ${token}`
+						}
+					}
+				);
+				const json = await response.json();
+				if (!response.ok || json.message) {
+					throw json.message || 'Something went wrong';
+				}
+				// alert(JSON.stringify(json));
+				setClips(createClipObjects(json));
+			} catch (error) {
+				console.log(error);
+			} finally {
+				//setFetchingItems(false);
+			}
+		}
+	}
+
+	const parseChannelData = (json) => {
+		let title = '';
+		if (json) {
+			const data = json.data;
+			if (data) {
+				const streamData = data[0];
+				if (streamData) {
+					title = streamData.title;
+				}
+			}
+		}
+		return title;
+	}
+
+	const createClipObjects = (json) => {
+		let clips = [];
+		if (json) {
+			clips = json.data.map(clipData =>
+				<div className='shadow'>
+					<a
+						href={clipData.url}
+						title={clipData.title}
+						target='_blank'
+					>
+						<img src={clipData.thumbnail_url} style={{ maxHeight: '100px' }} />
+					</a>
+				</div>
+			);
+		}
+		return clips;
+	}
+
 	return (
 		<Grid
 			fluid
@@ -18,12 +129,13 @@ const Index = () => {
 					<Grid fluid>
 						<Row>
 							<Col xs={12}>
-								<div class='twitch-content'>
+								<div className='twitch-content'>
 									<h1>
-										This is a stream title
+										{streamTitle.toUpperCase()}
 									</h1>
 									<TwitchPlayer
 										id='twitch-player'
+										className='shadow'
 										channel='tweak'
 										muted
 										height="100%"
@@ -52,6 +164,7 @@ const Index = () => {
 				>
 					<TwitchChat
 						id='twitch-chat'
+						className='shadow'
 						channel='tweak'
 						theme='dark'
 						height='100%'
